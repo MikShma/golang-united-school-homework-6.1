28d91d6f20909d134117e4320d922d284fdd28b7
MikShma <67111851+MikShma@users.noreply.github.com>
2022-05-28 23:23:25 +0300

    Update box.go

--- box.go
+++ box.go
@@ -27,39 +27,64 @@ func (b *box) AddShape(shape Shape) error {
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
-
+	if i > len(b.shapes) {
+		return Shape{},errors.New("shape by index doesn't exist or index went out of the range")
+	}
+	return b.shapes[i], nil
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
-
+	k, err := b.GetByIndex(i)
+	if err != nil {
+		return k, err
+	}
+	b.shapes = append( b.shapes[:i], b.shapes[i+1:]... ) 
+	return k, err
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
-
+	k, err := b.GetByIndex(i)
+	if err != nil {
+		return k, err
+	}
+	b.shapes[i] = shape
+	return k, err
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
-
+	var perim float64 = 0
+	for _, s := range b.shapes {
+		perim += s.CalcPerimeter()
+	}
+	return perim
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
-
+	var area float64 = 0
+	for _, s := range b.shapes {
+		area += s.CalcArea()
+	}
+	return area
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	panic("implement me")
-
+	var i interface{}
+	err := errors.New("circles are not exist in the list")
+	for k, s := range b.shapes {
+		i = s
+		_, ok := i.(Circle)
+		if ok {
+			_, err = b.ExtractByIndex(k)
+			k--
+		}
+	}
+	return err
 }
